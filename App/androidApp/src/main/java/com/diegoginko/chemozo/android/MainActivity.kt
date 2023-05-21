ed3c2c8336b630ccc83040de35f460d03b1141ed
Diego Ginko <diego.ginko@gmail.com>
2023-05-21 20:19:09 -0300

    Agrego biblioteca de mqtt

--- App/androidApp/src/main/java/com/diegoginko/chemozo/android/MainActivity.kt
+++ App/androidApp/src/main/java/com/diegoginko/chemozo/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.diegoginko.chemozo.android
 
+import MQTTClient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,8 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.diegoginko.chemozo.Greeting
+import mqtt.Subscription
+import mqtt.packets.Qos
+import mqtt.packets.mqttv5.SubscriptionOptions
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalUnsignedTypes::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +28,19 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        //Configuro recepcion de paquetes
+        val client = MQTTClient(
+            5,
+            "test.mosquitto.org",
+            1883,
+            null
+        ) {
+            println(it.payload?.toByteArray()?.decodeToString())
+        }
+        client.subscribe(listOf(Subscription("/randomTopic", SubscriptionOptions(Qos.EXACTLY_ONCE))))
+        client.publish(false, Qos.EXACTLY_ONCE, "/randomTopic", "hello".encodeToByteArray().toUByteArray())
+        client.run() // Blocking method, use step() if you don't want to block the thread
     }
 }
 
